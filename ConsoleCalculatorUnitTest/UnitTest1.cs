6146345bbc260d350f39447a8c93af6d0481d0db
DESKTOP-M25GUR8\Kostiantin <k.galchenko27@gmail.com>
2021-10-21 17:07:38 +0300

    !

--- ConsoleCalculatorUnitTest/UnitTest1.cs
+++ ConsoleCalculatorUnitTest/UnitTest1.cs
@@ -1,16 +1,16 @@
 using NUnit.Framework;
-
+using ConsoleApp1;
 namespace ConsoleCalculatorUnitTest
 {
     public class Tests
     {
         [SetUp]
         public void Setup()
-        {
-        }
-
+        {fdgsdg 
+        }sd
+           d
         [Test]
-        public void Test1()
+        public void AdditionTest (double firstArgument, double secondArgument, double expectedResult)
         {
             Assert.Pass();
         }
