c0c36c12faa2742d4e8f25a95933fb78f70704b3
DESKTOP-M25GUR8\Kostiantin <k.galchenko27@gmail.com>
2021-10-21 16:57:13 +0300

    Writting class main

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,7 +10,52 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Simple console calculator!\n");
+            Console.Write("Input first operand: ");
+            double firstOperand = Calc.NumInput();
+            Console.WriteLine();
 
+            Console.WriteLine("Input operator (+ - * / %): ");
+            bool isSecondNeed = Calc.OperatorInput(out string mathOperator);
+            double secondOperand = 0;
+            if (isSecondNeed = true)
+            {
+                Console.WriteLine("Input second operand: ");
+                secondOperand = Calc.NumInput();
+                Console.WriteLine();
+            }
+
+            double result = 0;
+            switch (mathOperator)
+            {
+                case "+":
+
+                    result = Calc.Sum(firstOperand, secondOperand);
+                    break;
+                case "-":
+
+                    result = Calc.Substract(firstOperand, secondOperand);
+                    break;
+
+                case "/":
+
+                    result = Calc.Divide(firstOperand, secondOperand);
+                    break;
+
+                case "*":
+
+                    result = Calc.Multiply(firstOperand, secondOperand);
+                    break;
+                case "%":
+
+                    result = Calc.RestOfDivide(firstOperand, secondOperand);
+                    break;
+
+                default:
+                    Console.WriteLine("No such opertor");
+                    break;
+            }
+            Console.WriteLine($"Yoyr result is: {result}");
         }
     }
 }
